e464cd4efd90cb0c1ed56b4cd5c47c28bfc8f1ca
Micaela <felder.micaela2009@gmail.com>
2020-09-25 20:46:19 +0200

    Agrego archivo obligatorio

---
+++ obligatorio1/obligatorio1/Gasto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace obligatorio1
+{
+    public class Gasto
+    {
+        public int Monto { get; set; }
+        public string Descripcion { get; set; }
+        public string Fecha { get; set; }
+        public Categoria  Categoria { get; set; }
+
+        public Gasto()
+        {
+            this.Monto = 0;
+            this.Descripcion = "No hay descripcion";
+            this.Fecha = "xx/xx/xxxx";
+        }
+
+        public bool ValidarDescripcion(string descripcion)
+        {
+            return true;
+        }
+    }
+}
