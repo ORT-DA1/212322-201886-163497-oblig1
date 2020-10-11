8482d1087d60524ab4c46c4b147792392259bd9d
Paula Hernandez <paulahk@gmail.com>
2020-10-11 16:39:39 -0300

    Se crea clase presupuesto y clase categoriaMonto para ese presupuesto

---
+++ obligatorio1/Test/CategoriaMontoPrueba.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Dominio;
+
+namespace Test
+{
+    [TestClass]
+    public class CategoriaMontoPrueba
+    {
+        private CategoriaMonto categoriaMonto;
+        private Categoria categoria;
+
+
+
+        [TestInitialize]
+        public void InitTests()
+        {
+            categoriaMonto = new CategoriaMonto();
+            categoria = new Categoria();
+        }
+
+        [TestMethod]
+        public void PropertyCategoriaPrueba()
+        {
+            categoriaMonto.Categoria = categoria;
+
+            Assert.AreEqual(categoria, categoriaMonto.Categoria);
+        }
+
+        [TestMethod]
+        public void PropertyMontoPrueba()
+        {
+            categoriaMonto.Monto = 1000;
+            int unMonto = 1000;
+            Assert.AreEqual(categoriaMonto.Monto, unMonto);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void MontoMayorA0Prueba()
+        {
+            categoriaMonto.Monto = -1000;
+        }
+
+        //en repositorio: Cada vez que se crea una categoria se crea un objeto CategoriaMonto de esa categoria.
+        [TestMethod]
+        public void MontoInicialEn0Prueba()
+        {
+            int montoInicial = 0;
+            Assert.AreEqual(montoInicial, categoriaMonto.Monto);
+
+        }
+
+    }
+}
---
+++ obligatorio1/obligatorio1/CategoriaMonto.cs
@@ -0,0 +1,49 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*  
+Si se agrega una categoría luego de haber creado el
+presupuesto, el valor para la categoría nueva será inicialmente cero,
+pudiendo el usuario cambiarlo después. Los montos por categoría son
+números positivos positivos, con dos decimales
+        }*/
+
+namespace Dominio
+{
+    public class CategoriaMonto
+    {
+
+        public Categoria Categoria { get; set; }
+
+        private float unMonto;
+        public float Monto
+        {
+
+            get { return unMonto; }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                else
+                {
+                    unMonto = value;
+                }
+
+            }
+
+        }
+        public CategoriaMonto()
+        {
+            this.Monto = 0;
+        }
+
+
+    }
+}
