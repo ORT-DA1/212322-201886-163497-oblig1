341fc872428a5f5f50b116af0bfb3052c3881723
micafelder <62482315+micafelder@users.noreply.github.com>
2020-10-24 17:49:46 +0200

    Administrador de Gastos Recurrentes creado

---
+++ obligatorio1/Test/AdministradorGastosRecurrentesPrueba.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Test
+{
+    [TestClass]
+    public class AdministradorGastosRecurrentesPrueba
+    {
+        private AdministradorGastosRecurrentes adminGastosRecurrentes;
+        private GastoRecuerrente unGastoRecuerrente;
+        private Categoria unaCategoria;
+        [TestInitialize]
+        public void InitTest()
+        {
+            adminGastosRecurrentes = new AdministradorGastosRecurrentes();
+            unGastoRecuerrente = new GastoRecuerrente();
+            unaCategoria = new Categoria();
+
+        }
+
+        [TestMethod]
+        public void RetornarListaGastosRecurrentesPrueba()
+        {
+
+            List<GastoRecuerrente> ListaLocal = new List<GastoRecuerrente>();
+            Assert.IsTrue(adminGastosRecurrentes.RetornarListaGastosRecurrentes().SequenceEqual(ListaLocal));
+
+        }
+
+        [TestMethod]
+        public void AgregarGastoComunConCategoriaDefinidaPrueba()
+        {
+            unaCategoria = new Categoria() { Nombre = "Entretenimiento" };
+            unGastoRecuerrente.Categoria = unaCategoria;
+            adminGastosRecurrentes.AgregarGastoRecurrente(unGastoRecuerrente);
+            Assert.IsFalse(adminGastosRecurrentes.EsVaciaListaGastosRecurrentes());
+        }
+    }
+}
---
+++ obligatorio1/obligatorio1/AdministradorGastosRecurrentes.cs
@@ -0,0 +1,39 @@
+﻿using Excepciones;
+using System;
+using System.Collections.Generic;
+
+namespace Dominio
+{
+    public class AdministradorGastosRecurrentes
+    {
+        private Repositorio Repositorio { get; }
+        public AdministradorGastosRecurrentes()
+        {
+            this.Repositorio = new Repositorio();
+
+        }
+
+        public List<GastoRecuerrente> RetornarListaGastosRecurrentes()
+        {
+            return Repositorio.RetornarListaGastosRecurrentes();
+        }
+        public void AgregarGastoRecurrente(GastoRecuerrente unGastoRecurrente)
+        {
+            if (NoHayCategoriaDefinida(unGastoRecurrente))
+            {
+                throw new ExcepcionElementoNoExistente("La categoria no puede quedar vacía");
+            }
+            else Repositorio.AgregarGastoRecurrente(unGastoRecurrente);
+
+        }
+        private bool NoHayCategoriaDefinida(GastoRecuerrente unGastoRecurrente)
+        {
+            return unGastoRecurrente.Categoria.Nombre == "No hay nombre";
+        }
+
+        public bool EsVaciaListaGastosRecurrentes()
+        {
+            return Repositorio.EsVaciaListaGastosRecurrentes();
+        }
+    }
+}
