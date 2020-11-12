0f509019c619ef8610b197a7fb8815a0dd11c11c
Paula Hernandez <paulahk@gmail.com>
2020-11-12 20:02:43 -0300

    creacion de Monedas

---
+++ obligatorio1/Test/AdministradorMonedaPrueba.cs
@@ -0,0 +1,47 @@
+﻿using Dominio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using obligatorio1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    [TestClass]
+    public class AdministradorMonedaPrueba
+    {
+        private Repositorio miRepositorio;
+        private AdministradorMonedas adminMonedas;
+        private Moneda unaMoneda;
+        private Moneda otraMoneda;
+
+        [TestInitialize]
+        public void InitTests()
+        {
+            miRepositorio = new Repositorio();
+            adminMonedas = new AdministradorMonedas(miRepositorio);
+            unaMoneda = new Moneda();
+            otraMoneda = new Moneda();
+        }
+
+        [TestMethod]
+        public void AgregarMonedaPrueba()
+        {
+            AdministradorMonedas.AgregarMoneda(unaMoneda);
+            List<Moneda> ListaLocal = new List<Moneda>();
+            ListaLocal.Add(unaMoneda);
+            Assert.AreEqual(ListaLocal, AdministradorMonedas.RetornarListaMonedas());
+        }
+
+        [TestMethod]
+        public void NoAgregarMonedaRepetidaPrueba()
+        {
+            AdministradorMonedas.AgregarMoneda(unaMoneda);
+
+        }
+    }
+
+
+}
---
+++ obligatorio1/obligatorio1/AdministradorMonedas.cs
@@ -0,0 +1,27 @@
+﻿
+using Dominio;
+using System;
+
+namespace obligatorio1
+{
+    public class AdministradorMonedas
+    {
+        private Repositorio Repositorio { get; }
+        public AdministradorMonedas(Repositorio unRepositorio)
+        {
+            this.Repositorio = unRepositorio;
+        }
+
+        public static void AgregarMoneda(Moneda unaMoneda)
+        {
+            if (this.Repositorio.ExisteMoneda(unaMoneda))
+            {
+                throw new ExcepcionElementoRepetido("Moneda ya existente");
+            }
+            else
+            {
+                Repositorio.AgregarMoneda(unaMoneda);
+            }
+        }
+    }
+}
