a854ee881ad485dd1ca8522a5e4a929455f8d64a
Paula Hernandez <paulahk@gmail.com>
2020-11-13 18:19:10 -0300

    interfaz moneda

---
+++ obligatorio1/Interfaz_De_Usuario/MenuArribaMoneda.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Dominio;
+
+namespace Interfaz_De_Usuario
+{
+    public partial class MenuArribaMoneda : UserControl
+    {
+        private AdministradorMonedas miAdminMonedas;
+        public MenuArribaMoneda(AdministradorMonedas unAdminMonedas)
+        {
+            InitializeComponent();
+            miAdminMonedas = unAdminMonedas;
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            panelNuevo.Controls.Clear();
+            UserControl agregarMoneda = new AgregarMoneda(miAdminMonedas);
+            panelNuevo.Controls.Add(agregarMoneda);
+        }
+    }
+}
