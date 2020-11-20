daea37a652dd09220212286e4d3777bbc5d4e001
Sara <saravila8@gmail.com>
2020-11-20 04:20:18 +0100

    ComienzoExportarGastos

---
+++ obligatorio1/obligatorio1/ExportarTxt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class ExportarTxt: ExportarReporteGasto
+    {
+        public override void Exportar(List<GastoComun> listaGastosComunes)
+        {
+            using(TextWriter tw = new StreamWriter(new FileStream(sfd.FileName, FileMode.Create), Encoding.UTF8))
+            {
+                foreach(GastoComun item in listaGastosComunes)
+                {
+                    await tw.WriteLineAsync(item.Fecha.Text + item.Categoria.Text);
+                }
+            }
+        }
+    }
+}
