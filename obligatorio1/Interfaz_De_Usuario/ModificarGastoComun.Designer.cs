142237aa8be29dea415908c6695da4de22d320dd
Paula <paulahk@gmail.com>
2020-11-25 14:06:50 -0300

    mas detalles

--- obligatorio1/Interfaz_De_Usuario/ModificarGastoComun.Designer.cs
+++ obligatorio1/Interfaz_De_Usuario/ModificarGastoComun.Designer.cs
@@ -50,16 +50,18 @@
             // cbGastoComun
             // 
             this.cbGastoComun.FormattingEnabled = true;
-            this.cbGastoComun.Location = new System.Drawing.Point(251, 108);
+            this.cbGastoComun.Location = new System.Drawing.Point(36, 98);
+            this.cbGastoComun.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbGastoComun.Name = "cbGastoComun";
-            this.cbGastoComun.Size = new System.Drawing.Size(317, 28);
+            this.cbGastoComun.Size = new System.Drawing.Size(589, 24);
             this.cbGastoComun.TabIndex = 31;
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(602, 103);
+            this.btnEliminar.Location = new System.Drawing.Point(631, 95);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(102, 36);
+            this.btnEliminar.Size = new System.Drawing.Size(91, 29);
             this.btnEliminar.TabIndex = 30;
             this.btnEliminar.Text = "Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
@@ -69,9 +71,9 @@
             // 
             this.lblSeleccionarGasto.AutoSize = true;
             this.lblSeleccionarGasto.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSeleccionarGasto.Location = new System.Drawing.Point(40, 110);
+            this.lblSeleccionarGasto.Location = new System.Drawing.Point(33, 68);
             this.lblSeleccionarGasto.Name = "lblSeleccionarGasto";
-            this.lblSeleccionarGasto.Size = new System.Drawing.Size(193, 26);
+            this.lblSeleccionarGasto.Size = new System.Drawing.Size(133, 18);
             this.lblSeleccionarGasto.TabIndex = 29;
             this.lblSeleccionarGasto.Text = "Seleccionar Gasto";
             // 
@@ -79,17 +81,18 @@
             // 
             this.lblTitulo.AutoSize = true;
             this.lblTitulo.Font = new System.Drawing.Font("MS Reference Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(35, 30);
+            this.lblTitulo.Location = new System.Drawing.Point(31, 24);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(368, 35);
+            this.lblTitulo.Size = new System.Drawing.Size(255, 24);
             this.lblTitulo.TabIndex = 28;
             this.lblTitulo.Text = "Modificar Gasto Comun";
             // 
             // btnModificarGasto
             // 
-            this.btnModificarGasto.Location = new System.Drawing.Point(602, 420);
+            this.btnModificarGasto.Location = new System.Drawing.Point(533, 353);
+            this.btnModificarGasto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnModificarGasto.Name = "btnModificarGasto";
-            this.btnModificarGasto.Size = new System.Drawing.Size(102, 36);
+            this.btnModificarGasto.Size = new System.Drawing.Size(91, 29);
             this.btnModificarGasto.TabIndex = 32;
             this.btnModificarGasto.Text = "Modificar";
             this.btnModificarGasto.UseVisualStyleBackColor = true;
@@ -99,49 +102,52 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(36, 158);
+            this.label1.Location = new System.Drawing.Point(30, 144);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(315, 26);
+            this.label1.Size = new System.Drawing.Size(217, 18);
             this.label1.TabIndex = 33;
             this.label1.Text = "Modificar Gasto Seleccionado:";
             // 
             // numMonto
             // 
             this.numMonto.DecimalPlaces = 2;
-            this.numMonto.Location = new System.Drawing.Point(251, 305);
+            this.numMonto.Location = new System.Drawing.Point(221, 262);
+            this.numMonto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.numMonto.Maximum = new decimal(new int[] {
             100000,
             0,
             0,
             0});
             this.numMonto.Name = "numMonto";
-            this.numMonto.Size = new System.Drawing.Size(317, 26);
+            this.numMonto.Size = new System.Drawing.Size(282, 22);
             this.numMonto.TabIndex = 40;
             // 
             // dtFecha
             // 
-            this.dtFecha.Location = new System.Drawing.Point(251, 364);
+            this.dtFecha.Location = new System.Drawing.Point(221, 309);
+            this.dtFecha.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dtFecha.MaxDate = new System.DateTime(2030, 12, 31, 0, 0, 0, 0);
             this.dtFecha.MinDate = new System.DateTime(2018, 1, 1, 0, 0, 0, 0);
             this.dtFecha.Name = "dtFecha";
-            this.dtFecha.Size = new System.Drawing.Size(317, 26);
+            this.dtFecha.Size = new System.Drawing.Size(282, 22);
             this.dtFecha.TabIndex = 38;
             // 
             // cbCategoria
             // 
             this.cbCategoria.FormattingEnabled = true;
-            this.cbCategoria.Location = new System.Drawing.Point(251, 425);
+            this.cbCategoria.Location = new System.Drawing.Point(221, 358);
+            this.cbCategoria.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbCategoria.Name = "cbCategoria";
-            this.cbCategoria.Size = new System.Drawing.Size(317, 28);
+            this.cbCategoria.Size = new System.Drawing.Size(282, 24);
             this.cbCategoria.TabIndex = 37;
             // 
             // lblCategoria
             // 
             this.lblCategoria.AutoSize = true;
             this.lblCategoria.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCategoria.Location = new System.Drawing.Point(104, 423);
+            this.lblCategoria.Location = new System.Drawing.Point(90, 356);
             this.lblCategoria.Name = "lblCategoria";
-            this.lblCategoria.Size = new System.Drawing.Size(109, 26);
+            this.lblCategoria.Size = new System.Drawing.Size(75, 18);
             this.lblCategoria.TabIndex = 36;
             this.lblCategoria.Text = "Categoria";
             // 
@@ -149,9 +155,9 @@
             // 
             this.lblFecha.AutoSize = true;
             this.lblFecha.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFecha.Location = new System.Drawing.Point(139, 364);
+            this.lblFecha.Location = new System.Drawing.Point(122, 309);
             this.lblFecha.Name = "lblFecha";
-            this.lblFecha.Size = new System.Drawing.Size(71, 26);
+            this.lblFecha.Size = new System.Drawing.Size(48, 18);
             this.lblFecha.TabIndex = 35;
             this.lblFecha.Text = "Fecha";
             // 
@@ -159,27 +165,28 @@
             // 
             this.lblMonto.AutoSize = true;
             this.lblMonto.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMonto.Location = new System.Drawing.Point(139, 302);
+            this.lblMonto.Location = new System.Drawing.Point(122, 260);
             this.lblMonto.Name = "lblMonto";
-            this.lblMonto.Size = new System.Drawing.Size(74, 26);
+            this.lblMonto.Size = new System.Drawing.Size(52, 18);
             this.lblMonto.TabIndex = 34;
             this.lblMonto.Text = "Monto";
             // 
             // tbDescripcion
             // 
-            this.tbDescripcion.Location = new System.Drawing.Point(251, 261);
+            this.tbDescripcion.Location = new System.Drawing.Point(221, 227);
+            this.tbDescripcion.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.tbDescripcion.MaxLength = 20;
             this.tbDescripcion.Name = "tbDescripcion";
-            this.tbDescripcion.Size = new System.Drawing.Size(317, 26);
+            this.tbDescripcion.Size = new System.Drawing.Size(282, 22);
             this.tbDescripcion.TabIndex = 42;
             // 
             // lblDescripcion
             // 
             this.lblDescripcion.AutoSize = true;
             this.lblDescripcion.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDescripcion.Location = new System.Drawing.Point(86, 259);
+            this.lblDescripcion.Location = new System.Drawing.Point(74, 225);
             this.lblDescripcion.Name = "lblDescripcion";
-            this.lblDescripcion.Size = new System.Drawing.Size(127, 26);
+            this.lblDescripcion.Size = new System.Drawing.Size(88, 18);
             this.lblDescripcion.TabIndex = 41;
             this.lblDescripcion.Text = "Descripcion";
             // 
@@ -187,23 +194,24 @@
             // 
             this.Moneda.AutoSize = true;
             this.Moneda.Font = new System.Drawing.Font("MS Reference Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Moneda.Location = new System.Drawing.Point(123, 210);
+            this.Moneda.Location = new System.Drawing.Point(107, 186);
             this.Moneda.Name = "Moneda";
-            this.Moneda.Size = new System.Drawing.Size(90, 26);
+            this.Moneda.Size = new System.Drawing.Size(62, 18);
             this.Moneda.TabIndex = 43;
             this.Moneda.Text = "Moneda";
             // 
             // cbMoneda
             // 
             this.cbMoneda.FormattingEnabled = true;
-            this.cbMoneda.Location = new System.Drawing.Point(251, 212);
+            this.cbMoneda.Location = new System.Drawing.Point(221, 188);
+            this.cbMoneda.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cbMoneda.Name = "cbMoneda";
-            this.cbMoneda.Size = new System.Drawing.Size(317, 28);
+            this.cbMoneda.Size = new System.Drawing.Size(282, 24);
             this.cbMoneda.TabIndex = 44;
             // 
             // ModificarGastoComun
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.cbMoneda);
             this.Controls.Add(this.Moneda);
@@ -221,8 +229,9 @@
             this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.lblSeleccionarGasto);
             this.Controls.Add(this.lblTitulo);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ModificarGastoComun";
-            this.Size = new System.Drawing.Size(827, 511);
+            this.Size = new System.Drawing.Size(735, 463);
             ((System.ComponentModel.ISupportInitialize)(this.numMonto)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
